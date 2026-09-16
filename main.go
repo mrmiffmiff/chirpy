ea2c42af569ced1eec650f545cc1c3132c859304
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-09-16 16:09:36 -0700

    better code clarity

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
+	server := &http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
