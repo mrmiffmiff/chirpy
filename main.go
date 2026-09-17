bd87422f6ac347c451f07ca2a7363e0ad361f904
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-09-16 20:48:23 -0700

    split readiness handler function out

--- main.go
+++ main.go
@@ -11,11 +11,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
 		Handler: mux,
@@ -24,4 +20,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
