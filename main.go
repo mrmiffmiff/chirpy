4dabe2525d6bdeb7237a2338070bdf1a95d7a5c3
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-09-18 10:13:14 -0700

    storage setup

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mrmiffmiff/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Something went wrong opening database: %v", err)
+	}
+	dbQueries := database.New(db)
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		dbQueries:      dbQueries,
+	}
+
 	const filePathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	cfg := &apiConfig{
-		fileserverHits: atomic.Int32{},
-	}
 	handlerFileserver := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
 	mux.Handle("/app/", cfg.middlewareMetricsInc(handlerFileserver))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
