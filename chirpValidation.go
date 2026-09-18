871d676cb174c50f41191728e5c2cc6b7d50f969
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-09-17 20:35:04 -0700

    chirp validation

---
+++ chirpValidation.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type errorResponseBody struct {
+	Error string `json:"error"`
+}
+
+type reqBody struct {
+	Body string `json:"body"`
+}
+
+type validResponseBody struct {
+	Valid bool `json:"valid"`
+}
+
+func handlerChirpValidation(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	requestBody := reqBody{}
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong with decoding the request")
+		return
+	}
+	if len(requestBody.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+	respondWithJSON(w, http.StatusOK, validResponseBody{
+		Valid: true,
+	})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respBody := errorResponseBody{
+		Error: msg,
+	}
+	data, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
